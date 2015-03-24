5edf28b1008a8fbbced3f2aa4b421188a76bc194
AlexMckey <mckey2002@yandex.ru>
2015-03-24 12:55:37 +0300

    Sources for solve problems by Scala

---
+++ src/main/scala/s99/ListsSolutions.scala
@@ -0,0 +1,39 @@
+package s99
+
+import Solutions._
+
+trait ListsSolutions {
+
+  def last[T](list: List[T]): T = ???
+  def penultimate[T](list: List[T]): T = ???
+  def nth[T](n: Int, list: List[T]): T = ???
+  def length[T](list: List[T]): Int = ???
+  def reverse[T](list: List[T]): List[T] = ???
+  def isPalindrome[T](list: List[T]): Boolean = ???
+  def flatten(list: List[Any]): List[Any] = ???
+  def compress[T](list: List[T]): List[T] = ???
+  def pack[T](list: List[T]): List[List[T]] = ???
+  def encode[T](list: List[T]): List[(Int, T)] = ???
+  def encodeModified[T](list: List[T]): List[Any] = ???
+  def decode[T](list: List[(Int, T)]): List[T] = ???
+  def encodeDirect[T](list: List[T]): List[(Int, T)] = ???
+  def duplicate[T](list: List[T]): List[T] = ???
+  def duplicateN[T](n: Int, list: List[T]): List[T] = ???
+  def drop[T](n: Int, list: List[T]): List[T] = ???
+  def split[T](n: Int, list: List[T]): (List[T], List[T]) = ???
+  def slice[T](i: Int, j: Int, list: List[T]): List[T] = ???
+  def rotate[T](n: Int, list: List[T]): List[T] = ???
+  def removeAt[T](i: Int, list: List[T]): (List[T], T) = ???
+  def insertAt[T](t: T, i: Int, list: List[T]): List[T] = ???
+  def range[T](i: Int, j: Int): List[Int] = ???
+  def randomSelect[T](n: Int, list: List[T]): List[T] = ???
+  def lotto[T](i: Int, j: Int): List[Int] = ???
+  def randomPermute[T](list: List[T]): List[T] = ???
+  def combinations[T](n: Int, list: List[T]): List[List[T]] = ???
+  def group3[T](list: List[T]): List[List[List[T]]] = ???
+  def groups[T](ns: List[Int], list: List[T]): List[List[List[T]]] = ???
+  def lsort[T](list: List[List[T]]): List[List[T]] = ???
+  def lsortFreq[T](list: List[List[T]]): List[List[T]] = ???
+
+}
+
